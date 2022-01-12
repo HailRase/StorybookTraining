9592be595f2791fc0a2e5b5f91c380e0478638f2
Юра <yurii_klimko@mail.ru>
2022-01-12 18:33:53 +0300

    train clearTimeout in useEffect

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 export default {
     title: 'useEffect demo'
@@ -30,15 +30,17 @@ export const SimpleExample = () => {
         </div>
     );
 };
-export const SetTimeoutExample = () => {
+export const SetIntervalExample = () => {
 
     const [date, setDate] = useState<Date>()
 
     useEffect(()=>{
-        setInterval(()=>{
+        const intervalId = setInterval(()=>{
             setDate(new Date())
         }, 1000)
-
+    return () => {
+            clearInterval(intervalId)
+    }
     },[])
 
 
@@ -50,3 +52,81 @@ export const SetTimeoutExample = () => {
     );
 };
 
+export const ResetEffectExample = () => {
+    const [fake, setFake] = useState(1)
+    const [count, setCount] = useState(1)
+
+    console.log('component rendered with ' + count)
+
+    useEffect(()=>{
+        console.log('Effect occurred: ' + count)
+
+        return () => {
+            console.log('Reset Effect with: ' + count)
+        }
+    },[count])
+    const incFake = useCallback(() => {
+        setFake(fake + 1)
+        console.log('create function incFake')
+    },[fake])
+    const incCount = useCallback(() => {
+        setCount(count + 1)
+        console.log('create function incCount')
+    },[count])
+
+    return (
+        <div>
+            <span>fake: {fake}</span>
+            <span>count: {count}</span>
+            <button onClick={incFake}>inc f</button>
+            <button onClick={incCount}>inc c</button>
+        </div>
+    );
+};
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState('')
+
+    console.log('component rendered with ' + text)
+
+    useEffect(()=>{
+        const handle = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+        window.addEventListener('keypress', handle)
+        return () => {
+            window.removeEventListener('keypress', handle)
+        }
+    },[text])
+
+
+    return (
+        <div>
+            <span>Typed text: {text}</span>
+        </div>
+    );
+};
+
+export const SetTimeoutExample = () => {
+
+    const [text, setText] = useState('')
+
+    useEffect(()=>{
+        const timeoutId = setTimeout(()=>{
+            setText('3 second passed')
+            console.log('3 second passed')
+        }, 3000)
+        return () => {
+            clearTimeout(timeoutId)
+        }
+    },[])
+
+
+
+    return (
+        <div>
+            <span>Text: {text}</span>
+        </div>
+    );
+};
